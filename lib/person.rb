44409c2c33bce05669adc1725fedc6675c2cd1df
vien pham <vienpham2019@gmail.com>
2020-01-28 15:27:56 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(fullname)
+    @name = fullname 
+  end 
+  def name 
+    @name 
+  end 
+  
+  def job=(jobType)
+    @job =  jobType
+  end
+  def job
+    @job
+  end
+end
