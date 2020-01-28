27238eb30ca5256bac49a5645cd64f1fcabfbc0b
vien pham <vienpham2019@gmail.com>
2020-01-28 15:25:30 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  def name=(fullName)
+    @name =  fullName
+  end
+  def name 
+    @name 
+  end
+  
+  def breed=(action)
+    @breed = action
+  end
+  def breed
+     @breed
+  end
+    
+end
